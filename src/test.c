212514c6becd08dbc83a2a09d8a0155c53a6c1a2
Dumonu <jacobcherry36@gmail.com>
2018-06-17 17:31:42 -0500

    Added test code for SDL_NumJoysticks()

--- src/test.c
+++ src/test.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include "SDL.h"
 
 int main()
 {
-    printf("hello, world!\n");
+    if(SDL_Init(SDL_INIT_GAMECONTROLLER))
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    while(1)
+    {
+        printf("%d\n", SDL_NumJoysticks());
+        SDL_Event e;
+        if(SDL_PollEvent(&e))
+        {
+            if(e.type == SDL_QUIT)
+                break;
+            if(e.type == SDL_KEYDOWN)
+            {
+                SDL_Keycode key = e.key.keysym.sym;
+                if(key == SDLK_ESCAPE)
+                {
+                    SDL_Event ex;
+                    ex.type = SDL_QUIT;
+                    SDL_PushEvent(&exit);
+                }
+            }
+        }
+    }
+
+    SDL_Quit();
 }
