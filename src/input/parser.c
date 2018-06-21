97a64dd0b1d6d5f9442d1319da75ea1ff578f8a6
Donovan <donovan.delaughter@gmail.com>
2018-06-21 14:26:43 -0500

    Finished the input reader.

--- src/input/parser.c
+++ src/input/parser.c
@@ -71,36 +71,74 @@ void removeTail(buffer_t player){
 		player.tail = mod((player.tail-1),bufferLength);
 	}
 }
-//Input: uint8_t array with the first element being the length of the array, player input buffer
-uint8_t searchInput(uint8_t* input, buffer_t buffer){
+//Input: uint8_t array of an input in anime notation with the first element being the length of the input, player input buffer
+uint8_t searchInput(uint8_t* input, buffer_t buffer, uint8_t flag)
+{
 	int length = input[0]
-	int currentIndex = 1;
+	int currentIndex = length;
+	int yPos;
+	int xPos;
+	int fiveCount;
 	//Every possible joystick position in the game defined by a 2D array.
-	int Inputs[3][3] = {  
+	int possibleInputs[3][3] = 
+	{  
    		{7,8,9} ,   
 		{4,5,6} ,   
 		{1,2,3}   
 	};
-	int yPos;
-	int xPos;
-
-	for (int i = buffer.head; i != buffer.tail; i= mod((i+1), bufferLength)){
-		yPos = 1;
-		xPos = 1;
-		if(inputs[i][0]==1||inputs[i][0]==2){
-			yPos++;
+	
+	//Scan the entire buffer
+	for (int i = buffer.tail; i != buffer.head; i= mod((i-1), bufferLength))
+	{
+		if(buffer.inputs[i][buttonCount+1] == 1 && !flag)
+		{
+			return 0;
+		else
+		{
+			//Use inputs to calculate what direction is being held
+			yPos = 1;
+			xPos = 1;
+			if(buffer.inputs[i][0]==1||buffer.inputs[i][0]==2)
+			{
+				yPos++;
+			}
+			if(buffer.inputs[i][1]==1||buffer.inputs[i][1]==2)
+			{
+				yPos--;
+			}
+			if(buffer.inputs[i][2]==1||buffer.inputs[i][2]==2)
+			{
+				xPos--;
+			}
+			if(buffer.inputs[i][3]==1||buffer.inputs[i][3]==2)
+			{
+				xPos++;
+			}
+			//Give up on the input if more than fiveLimit fives are consecutive *unless* that five is required by the input. 
+			if(possibleInputs[yPos][xPos]==5 && input[currentIndex] != possibleInputs[yPos][xPos])
+			{
+				fiveCount++;
+				if(fiveCount >= fiveLimit){
+				return 0;
+			}
+			//Wasn't a five or five was needed.
+			}else
+			{
+				fiveCount = 0;
+				if (input[currentIndex] == possibleInputs[yPos][xPos])
+				{
+					currentIndex--;
+					if (currentIndex = 0)
+					{
+						buffer.inputs[i][buttonCount+1] = 1;
+						return 1;
+					}	
+				}
+			}
 		}
-		if(inputs[i][1]==1||inputs[i][1]==2){
-			yPos--;
-		}
-		if(inputs[i][2]==1||inputs[i][2]==2){
-			xPos--;
-		}
-		if(inputs[i][3]==1||inputs[i][3]==2){
-			xPos++;
-		}
-
 	}
+	//Input wasn't found until now. Just return 0.
+	return 0;
 }
 void parserUpdate(){
 	add(joyStatep1(), Buffers[0]);
