96c6283633eb7ce68176f34758cc0f4eb604e803
Dumonu <jacobcherry36@gmail.com>
2018-07-11 23:55:43 -0500

    Fixed typo

--- src/input/parser.c
+++ src/input/parser.c
@@ -43,7 +43,7 @@ void initQueues()
 // Remove Buffers from memory.
 void destroyBuffers() {
     for (int i = 0; i < 2; i++) {
-        for(int i = 0; i < bufferLength; i++){
+        for(int i = 0; i < queueLength; i++){
             if(Queues[i].inputs[i] != NULL)
                 free(Queues[i].inputs[i]);
         }
